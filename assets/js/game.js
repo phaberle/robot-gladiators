e35c5e3d0fbe9eeb6ab54a6fcecb1aeea4063ea5
Paul Haberle <61989020+phaberle@users.noreply.github.com>
2022-04-01 13:49:36 -0500

    playing with js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log(10 + 10);
+console.log("Our robot's name is " + playerName);
+
+var name = "your name";
+console.log(name);
+
+/*function fight() {
+window.alert("The fight has begun!");
+}
+*/
